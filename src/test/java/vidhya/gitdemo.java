e336160185edf04bc8a7c2e6ea991cb6367b95a9
rajes <rajes@DESKTOP-LAFM9KP>
2023-11-17 08:47:39 +0400

    tc_002 update

---
+++ src/test/java/vidhya/gitdemo.java
@@ -0,0 +1,7 @@
+package vidhya;
+
+public class gitdemo {
+public static void main(String[] args) {
+	System.out.println("git practise");
+}
+}
---
+++ src/test/java/vidhya/srisri.java
@@ -0,0 +1,7 @@
+package vidhya;
+
+public class srisri {
+public static void main(String[] args) {
+	System.out.println();
+}
+}
